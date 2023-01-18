e580f47b11b079bb8a8921f7f588650bf0743c43
zinyblaq <nwambaella@gmail.com>
2023-01-18 20:34:57 +0100

    day8

--- src/pages/companypage.js
+++ src/pages/companypage.js
@@ -4,6 +4,9 @@ import Header from '../header';
 import Footer from '../footer';
 import HomeFeatures from '../text-image';
 import heroimg from './HeroImage.png';
+import familyimg from './family.png';
+import computerimg from './computerwork.png';
+import shoeimg from './shoes.png';
 
 export default function CompanyPage() {
   return (
@@ -18,6 +21,24 @@ export default function CompanyPage() {
           </div>
           <div className="landing-1-2">
             <HomeFeatures image={heroimg} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>
+          We provide a complete software platform for managing subscription
+          services
+        </h1>
+      </div>
+      <div className="landing">
+        <div className="landing-1">
+          <div className="landing-1-1">
+            <div>
+              <h1>Subscription Management Made Easy!</h1>
+            </div>
+          </div>
+          <div className="landing-1-2">
+            <HomeFeatures image={computerimg} />
           </div>
         </div>
       </div>
