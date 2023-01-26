b908e2339a67325bfb178d898e7a04c710b79b26
zinyblaq <nwambaella@gmail.com>
2023-01-26 20:52:10 +0100

    day14

---
+++ src/featured-fun.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './featured-fun.css';
+import blackwoman from './blackwoman.jpg';
+
+export default function FeaturedFun(image, h1, p, lists) {
+  return (
+    <div>
+      <div>
+        <img src={blackwoman} alt="yes" />
+      </div>
+    </div>
+  );
+}
--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -4,21 +4,60 @@ import Footer from '../footer';
 import './how-it-works.css';
 import HomeFeatures from '../text-image';
 import handsonlaptop from './HIWhands.png';
+import FeaturedFun from '../featured-fun';
 
 export default function HowItWorks() {
   return (
     <div>
       <Header />
-      <h1>We Focus on Structured and Sustainable Subscription Management.</h1>
-      <div>
+      <div className="HIW-main-section">
+        <h1 className="firstheader">
+          We Focus on Structured and Sustainable Subscription Management.
+        </h1>
+        <div className="HIW-border"></div>
+      </div>
+      <div className="HIW-section1">
         <HomeFeatures
           text1={
-            'A Platform you can trust to give you seamless and stress-free experience all day everyday.”'
+            <p
+              style={{
+                color: '#030640',
+                width: '493px',
+                height: '131px',
+                fontSize: '28px',
+                // lineHeight: '30px',
+                fontWeight: '500',
+                lineHeight: '56px',
+                letterSpacing: '-0.01em',
+              }}
+            >
+              'A Platform you can trust to give you seamless and stress-free
+              experience all day everyday.'
+            </p>
           }
-          text2="You want to know how flex works? We’ve got you.
-      Take a quick tour around Flex and see how it works. Sign up to get started!"
+          text2={
+            <p
+              style={{
+                color: 'rgba(51, 51, 51, 0.9)',
+                width: '490px',
+                height: '86px',
+                fontSize: '18px',
+                // lineHeight: '30px',
+                fontWeight: '400',
+                lineHeight: '28px',
+                marginTop: '60px',
+              }}
+            >
+              `You want to know how flex works? We’ve got you. Take a quick tour
+              around Flex and see how it works. Sign up to get started!`
+            </p>
+          }
           image={handsonlaptop}
         />
+        <button className="button">WATCH VIDEO </button>
+      </div>
+      <div>
+        <FeaturedFun />
       </div>
       <Footer />
     </div>
