d7ccfb49719e845d8ed4ecde1d8b378933d17322
zinyblaq <nwambaella@gmail.com>
2023-01-27 20:52:51 +0100

    day15

--- src/featured-fun.js
+++ src/featured-fun.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './featured-fun.css';
-import blackwoman from './blackwoman.jpg';
+import blackwoman from './pages/HIWlaptop.png';
+import icons from './pages/HIWicon2.png';
 
 export default function FeaturedFun(image, h1, p, lists) {
   return (
-    <div>
-      <div>
+    <div className="main-featuredfun">
+      <div className="background-line">
         <img src={blackwoman} alt="yes" />
+      </div>
+      <div className="featuredfun-text">
+        <h1>Our Featured Functionalities</h1>
+        <p>Here’re some of the features Flex has to offer.</p>
+        <ul>
+          <li>
+            <div className="featuredfun-text-lists">
+              <div>
+                <img src={icons} alt="icon" />
+              </div>
+              <h3 className="featured-text-header3">
+                Marketing tools & Discounts and promos
+              </h3>
+            </div>
+          </li>
+          <li>
+            <div className="featuredfun-text-lists">
+              <div>
+                <img src={icons} alt="icon" />
+              </div>
+              <h3 className="featured-text-header3">
+                Marketing tools & Discounts and promos
+              </h3>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
